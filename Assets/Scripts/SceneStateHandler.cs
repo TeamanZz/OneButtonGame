9b0674cf711e49944ad2abfd17bce624f7ac5f8e
TeamanZz <a.yasinko@mail.ru>
2022-11-21 04:40:13 +0300

    Speech Controller

--- Assets/Scripts/SceneStateHandler.cs
+++ Assets/Scripts/SceneStateHandler.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneStateHandler : MonoBehaviour
 {
+    public int levelIndex = 0;
+    public OnInGameEvent OnLevelStart;
+
+    private void Start()
+    {
+        OnLevelStart.Invoke(levelIndex, GameEventType.LevelStart);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
@@ -12,4 +21,8 @@ public class SceneStateHandler : MonoBehaviour
             SceneManager.LoadScene(0);
         }
     }
+
 }
+
+[System.Serializable]
+public class OnInGameEvent : UnityEvent<int, GameEventType> { }
--- Assets/Scripts/SpeechController.cs
+++ Assets/Scripts/SpeechController.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class SpeechController : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private int speechIndex;
-    [SerializeField] private List<AudioClip> speechsList = new List<AudioClip>();
+    [SerializeField] private List<LevelSpeeches> levelsSpeechesList = new List<LevelSpeeches>();
 
-    private void Start()
+    private SceneStateHandler sceneStateHandler;
+
+    private void Awake()
     {
-        Invoke("PlaySpeech", 2);
+        sceneStateHandler = GetComponent<SceneStateHandler>();
+        sceneStateHandler.OnLevelStart.AddListener(CheckOnSpeech);
     }
 
-    public void PlaySpeech()
+    public void CheckOnSpeech(int levelIndex, GameEventType eventType)
     {
-        audioSource.PlayOneShot(speechsList[speechIndex]);
+        SpeechEvent speechEvent = levelsSpeechesList[levelIndex].levelSpeeches.Find(x => x.eventType == eventType);
+        PlaySpeech(speechEvent.audioClip);
     }
+
+    public void PlaySpeech(AudioClip audioClip)
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
+
+[System.Serializable]
+public class LevelSpeeches
+{
+    public List<SpeechEvent> levelSpeeches = new List<SpeechEvent>();
+}
+
+[System.Serializable]
+public class SpeechEvent
+{
+    public GameEventType eventType;
+    public AudioClip audioClip;
+}
+
+public enum GameEventType
+{
+    LevelStart,
+    LevelRestart,
+    PlayerWin,
+    PlayerLose,
 }
